61e3c4007e482e73de84f86062ca1f5803750905
Jared Thomas <sci.imagino@gmail.com>
2020-11-29 17:31:35 -0800

    Prints out calculated area from gauge diameters

--- FreeElectron/main.c
+++ FreeElectron/main.c
@@ -2,9 +2,18 @@
 #include <math.h>
 
 
+#define PI 3.14159
+
+
 double calculateDiameter(int awg)
 {
     return 0.127 * pow(92, (36.0 - awg) / 39);
+}
+
+
+double calcArea(double diameter)
+{
+    return PI * pow(diameter / 2.0, 2);
 }
 
 
@@ -13,5 +22,9 @@ int main(int argc, char **argv)
     int awg;
     for(awg = 0; awg <= 36; awg++) {
         printf("%d: %.*F\n", awg, 3, calculateDiameter(awg));
+    }
+    fputc('\n', stdout);
+    for(awg = 0; awg <= 36; awg++) {
+        printf("%d: %.*F\n", awg, 3, calcArea(calculateDiameter(awg)));
     }
 }
