bf4a497306967565cb1dd008dd4d14cbb8889a78
Jared Thomas <sci.imagino@gmail.com>
2020-12-13 05:51:35 -0800

    Done with prompt.c for the most part; need to add func def

--- FreeElectron/prompt.c
+++ FreeElectron/prompt.c
@@ -1,1 +1,20 @@
-﻿
+﻿#include <stdio.h>
+
+#include "prompt.h"
+
+
+char *prompt(const char *msg, char *buffer, size_t num_chars)
+{
+    fputs(msg, stdout);
+    fgets(buffer, num_chars, stdin);
+    removeNewline(buffer);
+    return buffer;
+}
+
+
+char prompt_quit(
+    const char *msg, char *buffer, size_t num_chars, const char *quit_str)
+{
+    if(strcmp(prompt(msg, buffer, num_chars), quit_str)) return 0;
+    else return 1;
+}
